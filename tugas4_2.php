7b1799d8bd44bcd14080c6a10d3057e9219395a5
Ciel <danagames30@gmail.com>
2021-12-12 18:59:44 +0700

    Initial commit

---
+++ tugas4_2.php
@@ -0,0 +1,9 @@
+<?php 
+    $saluran = 523;
+    $terpakai = 8891;
+    $rata_rata = $terpakai/$saluran;
+     echo "Jumlah Saluran = ".$saluran;
+     echo "<br> Jumlah Terpakai = ".$terpakai;
+     echo "<br> Rata-rata Pemakaian = ...?";
+     echo "<br> Jawaban : ".$rata_rata." liter/keluarga";
+?>
